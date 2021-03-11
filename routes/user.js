e76a37df6b692999411052392a199b32e12ad73b
fernando <fernom95@gmail.com>
2021-03-11 13:48:28 -0600

    send mail de referencia

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const Product = require('../models/Product.model');
 const Order = require('../models/Order.model');
 const Kits = require('../models/Kits.model')
 const router  = express.Router();
+const nodemailer = require("nodemailer")
+
 
 //aqui poner el auth
 router.get('/:category', async (req, res, next) => {
@@ -47,7 +49,28 @@ router.get("/result/:kit" , async (req,res,next)=>{
     res.json(elKit)
 })
 
+router.post("/sendmail", async (req,res,next)=>{
+    const {emailAddress} = req.body
 
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'geratref15@gmail.com',
+          pass: 'NOMBRESFALSOS123'
+        }
+      });
+      transporter.sendMail({
+        from: '" Helpy - creando y mejorando tu espacio " <info@helpy.mx>',
+        to: emailAddress, 
+        subject: "Apoyo para Home Office", 
+        text: "Tener un espacio de trtabajo y las herramientas, es indispensable para realizar un trabajo bien hecho, es por eso que estan pidiendo tu apoyo para comprar este espacio de trabajo",
+        html: `<b>${text}</b>`
+      })
+      .then(info => console.log(info))
+      .catch(error => console.log(error))
+
+
+})
 
 module.exports = router;
 
